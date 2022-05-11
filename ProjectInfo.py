465e0fdbcdda30297b233eedee31232233ea5aa3
“rajatt95” <rajatvermaa95@gmail.com>
2022-05-12 01:07:43 +0530

    Initial commit

---
+++ ProjectInfo.py
@@ -0,0 +1,16 @@
+# /**
+# * @author Rajat Verma
+# * https://www.linkedin.com/in/rajat-v-3b0685128/
+# * https://github.com/rajatt95
+# * https://rajatt95.github.io/
+# *
+# * Course: Learn API Automation Testing with Python & BDD Framework (https://www.udemy.com/course/python-sdet-rest-api-automation/)
+# * Tutor: Rahul Shetty (https://www.udemy.com/user/rahul445/)
+# */
+#
+# /***************************************************/
+
+# /** * Project name: PythonRequests_BDD
+# * https://github.com/rajatt95/PythonRequests_BDD *
+#
+# About: Automation Testing | API | Requests | Python | PyCharm | BDD | Cucumber | Hooks | Parameterization | Tagging | Allure Reports */
